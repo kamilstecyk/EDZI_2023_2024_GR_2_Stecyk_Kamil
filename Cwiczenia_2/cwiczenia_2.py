162a2ce4e6977bd2ef8aee1c0277f84afaee5a1f
Kamil Stecyk <kamils987@interia.eu>
2024-03-15 16:17:48 +0100

    add getting again random website if there are no links

--- Cwiczenia_2/cwiczenia_2.py
+++ Cwiczenia_2/cwiczenia_2.py
@@ -15,9 +15,19 @@ links = get_links(url)
 
 current_link = links[random.randint(0, len(links) - 1)]
 print(current_link)
+how_many_links = 1
 
 for i in range(0, 99):
+    while len(get_links(current_link)) == 0:
+        current_link = links[random.randint(0, len(links) - 1)]
+        # print('Again get new random')
+
     links = get_links(current_link)
+    # print('how many links: ', len(links))
     current_link = links[random.randint(0, len(links) - 1)]
     print(current_link)
+
+    how_many_links += 1
+
+print('How many links has been printed: ', how_many_links)
 
